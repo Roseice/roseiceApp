4bca5c13c3ca47ba4e9c4d52b837d24e67c78817
Roseice <roseice910528@gmail.com>
2022-09-06 12:01:07 +0800

    roseice
    test

--- roseiceApp/Program.cs
+++ roseiceApp/Program.cs
@@ -10,6 +10,12 @@ namespace roseiceApp
     {
         static void Main(string[] args)
         {
+            string line;//宣告變數為line
+            Console.WriteLine("Hello World!");//顯示Hello World!
+            Console.WriteLine("請輸入姓名");
+            line = Console.ReadLine();//獲取使用者輸入存於line
+            Console.WriteLine("你好"+line);//顯示line儲存資料
+            Console.ReadLine();//按Enter結束
         }
     }
 }
